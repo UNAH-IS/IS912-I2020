3da9ab871d314e8ef8f5ed1ce2d18dc6a4d75347
Erick Marín <erick.marin.v@gmail.com>
2020-02-02 13:27:22 -0600

    Hola mundo, netflix

--- netflix/index.js
+++ netflix/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('www'));//use se utiliza para ejecutar middlewares
+app.use(express.static('public'));//use se utiliza para ejecutar middlewares
 
 app.listen('4200',function(){
     console.log('Servidor levantado')
