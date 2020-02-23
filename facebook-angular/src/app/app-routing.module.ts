4de5d4c74e917f4c28f823661e79565c4959646b
Erick Marín <erick.marin.v@gmail.com>
2020-02-22 20:15:50 -0600

    Ejercicio Facebook

--- facebook-angular/src/app/app-routing.module.ts
+++ facebook-angular/src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'ab*cd',
+    redirectTo: '/register',
+    pathMatch: 'full'
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- facebook-angular/src/app/app.module.ts
+++ facebook-angular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegisterComponent,
+    FooterComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
