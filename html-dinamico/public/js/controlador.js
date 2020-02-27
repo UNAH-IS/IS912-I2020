03e7fe5d3622c7c82331cd160a1a8b4b2d777d1e
Erick Marín <erick.marin.v@gmail.com>
2020-02-27 17:57:18 -0600

    Javascript DOM

--- html-dinamico/public/js/controlador.js
+++ html-dinamico/public/js/controlador.js
@@ -12,3 +12,54 @@ let parrafos = document.querySelectorAll('p');
 
 for(let i = 0; i<parrafos.length;i++)
     console.log(parrafos[i].innerHTML);
+
+
+function cambiarContenido(){
+    document.getElementById('p1').innerHTML = '<b>Nuevo contenido</b>';
+}
+
+function cambiarEstilos(){
+    document.getElementById('p2').style.backgroundColor = '#000000';
+    document.getElementById('p2').style.color = 'red';
+    document.getElementById('p2').style.fontFamily = 'Roboto';
+
+}
+
+function activarParrafo(etiqueta){
+    etiqueta.classList.remove('parrafo-inactivo');
+    etiqueta.classList.add('parrafo-activo');
+}
+
+function inactivarParrafo(etiqueta){
+    etiqueta.classList.remove('parrafo-activo');
+    etiqueta.classList.add('parrafo-inactivo');
+}
+
+function login(){
+    validarCampoVacio('usuario');
+    validarCampoVacio('password');
+}
+
+function validarCampoVacio(id){
+    const etiqueta = document.getElementById(id);
+    if (etiqueta.value==''){
+        etiqueta.classList.remove('input-success');
+        etiqueta.classList.add('input-error');
+    }else{
+        etiqueta.classList.remove('input-error');
+        etiqueta.classList.add('input-success');
+    }
+}
+
+function validarCorreo(etiqueta){
+    console.log(etiqueta.value);
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(etiqueta.value)){
+        etiqueta.classList.remove('input-error');
+        etiqueta.classList.add('input-success');
+        document.getElementById('btn-login').disabled=false;
+    } else{
+        etiqueta.classList.remove('input-success');
+        etiqueta.classList.add('input-error');
+    }
+}
