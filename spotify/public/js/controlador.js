feba87a8b0b69d420df0fa31ac5d5445bf5d6be5
Erick Marín <erick.marin.v@gmail.com>
2020-03-05 21:17:00 -0600

    Ejercicio Spotify

--- spotify/public/js/controlador.js
+++ spotify/public/js/controlador.js
@@ -1,3 +1,9 @@
+// function verPlaylist(codigoPlaylist){
+//     console.log('Ver playlist con codigo: ' + codigoPlaylist);
+//     $("#vista-playlist").show();
+//     $("#vista-artista").hide();
+// }
+
 var artistas =[];
 var localStorage = window.localStorage;
 var artistaSeleccionado;
@@ -98,64 +104,60 @@ else{
     artistas = JSON.parse(localStorage.getItem('artistas'));
 }
 
-function generarTablaArtistas(){
-    document.querySelector('#tabla-artistas tbody').innerHTML = '';
-    for(let i=0;i<artistas.length;i++)
-        document.querySelector('#tabla-artistas tbody').innerHTML +=
-                        `<tr>
-                            <td>${artistas[i].nombreArtista}</td>
-                            <td><img class="caratula" src="${artistas[i].caratulaArtista}"></td>
-                            <td>${artistas[i].albumes.length}</td>
-                            <td><button type="button" onclick="verDetallesArtista(${i});">Ver detalles</button></td>
-                        </tr>`;
-
-}
-
-generarTablaArtistas();
-
-
-function  verDetallesArtista(indice){
-    artistaSeleccionado = indice;
-    console.log("Ver detalles del artista?" + indice);
-    console.log(artistas[indice]);
-    document.getElementById('span-artista').innerHTML = artistas[indice].nombreArtista
-    document.querySelector('#tabla-albumes tbody').innerHTML = '';
-    for (let j=0;j<artistas[indice].albumes.length; j++){
-        const album = artistas[indice].albumes[j];
-        document.querySelector('#tabla-albumes tbody').innerHTML +=
-            `<tr>
-                <td>${album.tituloAlbum}</td>
-                <td>${album.anio}</td>
-                <td>${album.genero}</td>
-                <td>${album.caratula}</td>
-                <td>${album.canciones.length}</td>
-                <td><button onclick="verCanciones(${j})">Ver canciones</button></td>
-            </tr>`;
-        }
-}
-
-function guardarArtista(){
-    let artista = {
-        nombreArtista:document.getElementById('nombre-artista').value,
-        caratulaArtista:document.getElementById('caratula-artista').value,
-        albumes:[]
+function generarListaArtistas(){
+    document.getElementById('lista-artistas').innerHTML = '';
+    for(let i=0;i<artistas.length;i++){
+        document.getElementById('lista-artistas').innerHTML +=
+            `<li class="nav-item">
+                <div class="nav-link" onclick="verArtista(${i})">
+                    <i class="fas fa-music"></i> ${artistas[i].nombreArtista}
+                </div>
+            </li>`;
     }
-
-    console.log(artista);
-    artistas.push(artista);
-    localStorage.setItem('artistas',JSON.stringify(artistas));
-    generarTablaArtistas();
 }
 
-function limpiarLocalStorage(){
-    localStorage.clear();
+generarListaArtistas();
+
+function verArtista(indiceArtista){
+    artistaSeleccionado = indiceArtista;
+    console.log(artistas[indiceArtista]);
+    
+    $("#vista-artista").show();
+    $("#vista-playlist").hide();
 }
 
-function verCanciones(indiceAlbum){
-    console.log("Artista: " + artistaSeleccionado + ", Album: " + indiceAlbum);
-    const album = artistas[artistaSeleccionado].albumes[indiceAlbum];
-    document.getElementById('lista-canciones').innerHTML = '';
-    for (let i=0;i<album.canciones.length;i++)
-        document.getElementById('lista-canciones').innerHTML +=
-            `<li>${album.canciones[i].nombreCancion}(${album.canciones[i].duracion})</li>`;
+function agregarCancion(codigoCancion){
+    console.log("Agregar cancion "+codigoCancion);
+    $("#modal-playlists").modal('show');
 }
+
+
+
+
+let temp = `<section class="container-fluid">
+<div class="row">
+  <div class="col-4 text-center">
+    <div class="cover-image" style="background-image:url(img/cover1.jpg);">
+    </div><br>
+    <h5 class="text-muted">Album 1</h5>
+    <button class="btn btn-success"type="button">Play</button>
+  </div>
+  <div class="col-8">
+    
+    <!--Item 1 -->
+    <div class="row song-item">
+      <div class="col-1"><i class="fas fa-play"></i></div>
+      <div class="col-10">
+        <div class="song-title">Cancion</div>
+        <div class="song-description">Artista - Album</div>
+      </div>
+      <div class="col-1">
+          <span>3:56</span>
+          <button onclick="agregarCancion(1)" class="btn btn-outline-success btn-sm" title="Agregar a playlist"><i class="fas fa-plus"></i></button>
+      </div>
+    </div>
+    
+  </div>
+</div>
+</section>
+<hr>`;
